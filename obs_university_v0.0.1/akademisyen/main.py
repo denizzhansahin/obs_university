9c2fe6281534c802df916b9af38a863e0051189a
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2022-12-21 19:13:07 +0300

    V0.0.1 Bilgiler
    Bilgiler.

---
+++ obs_university_v0.0.1/akademisyen/main.py
@@ -0,0 +1,28 @@
+# This Python file uses the following encoding: utf-8
+import sys
+import os
+
+
+from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
+
+
+class OBS_Akademisyen(QMainWindow):
+    def __init__(self):
+        super(OBS_Akademisyen, self).__init__()
+        self.load_ui()
+
+    def load_ui(self):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__), "form.ui")
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        loader.load(ui_file, self)
+        ui_file.close()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = OBS_Akademisyen()
+    widget.show()
+    sys.exit(app.exec_())
